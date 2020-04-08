131c93cad4fba591f62d100c1e1f0414939880c0
vagrant <vagrant@localhost.localdomain>
2020-04-08 09:56:30 +0000

    [Add] バリデーション・エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book)
+  	@books = Book.all.order(id: :ASC)
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(@book)
+  	else
+  		render action: :index
+  	end
   end
 
   def show
@@ -19,9 +23,12 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		redirect_to book_path(@book)
+  	else
+  		render action: :edit
+  	end
   end
 
   def destroy
